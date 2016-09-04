0084417feba2b8e7fc2e83161ec3f6e5af77d412
Anthony BARA <banthony@e2r12p12.42.fr>
2016-09-04 18:18:14 +0200

    Amelioration ft_printtab

--- ft_printtab.c
+++ ft_printtab.c
@@ -6,13 +6,13 @@
 /*   By: banthony <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 12:55:08 by banthony          #+#    #+#             */
-/*   Updated: 2016/01/22 15:02:14 by banthony         ###   ########.fr       */
+/*   Updated: 2016/09/04 16:12:07 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_printtab(char **tab)
+void	ft_printtab(char **tab, void (*f)(const char *s), char *str)
 {
 	int i;
 
@@ -23,6 +23,8 @@ void	ft_printtab(char **tab)
 	{
 		if (tab[i] == NULL)
 			return ;
+		if ((f && str) && tab[i] != NULL)
+			f(str);
 		ft_putstr(tab[i]);
 		i++;
 	}
