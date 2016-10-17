6c108a9d7a5b765b9c4826b3db6c7db1ab071a67
Anthony BARA <banthony@e2r2p17.42.fr>
2016-10-17 16:57:33 +0200

    suppr ligne en trop

--- ft_strjoin_replace.c
+++ ft_strjoin_replace.c
@@ -6,7 +6,7 @@
 /*   By: banthony <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 19:25:09 by banthony          #+#    #+#             */
-/*   Updated: 2016/09/22 15:43:09 by banthony         ###   ########.fr       */
+/*   Updated: 2016/10/17 16:56:23 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_strjoin_replace(char *dest, char *src)
 	char *tmp;
 
 	tmp = NULL;
-
 	tmp = ft_strjoin(dest, src);
 	ft_strdel(&dest);
 	dest = ft_strdup(tmp);
